30dca3e81c349c8d813ceaefee75dc2deecf9b1f
tkasa01 <kasatanya1@gmail.com>
2018-11-07 12:40:13 +0000

    Comments file

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,54 +1,55 @@
 package main.java.models;
 
-//import javax.persistence.Entity;
-import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
 
-//@Entity
-@XmlRootElement(name="seller")
 public class Person {
+    private int id;
     private String firstName;
     private String lastName;
-    private int id;
     private String phoneNumber;
     private String email;
+    private Date startDate;
 
     public Person(){}
-    public Person(String newFirstName, String newLastName , int newId, String newEmail){
+    public Person(int newId, String newFirstName, String newLastName,  String newNumber, String newEmail){
+        id = newId;
         firstName = newFirstName;
         lastName = newLastName;
-        id = newId;
         email = newEmail;
+        startDate = new Date();
     }
 
     //Getters
+    public int getId(){
+        return id;
+    }
     public String getFirstName(){
         return firstName;
     }
     public String getLastName(){
         return lastName;
     }
-    public int getId(){
-        return id;
-    }
     public String getPhoneNumber(){
         return phoneNumber;
     }
     public String getEmail(){return email;}
+    public Date getStartDate(){return startDate;}
 
     //Setters
+    public void setId(int id){
+        this.id = id;
+    }
     public void setFirstName(String firstName){
         this.firstName  = firstName;
     }
     public void setLastName(String lastName){
         this.lastName = lastName;
     }
-    public void setId(int id){
-        this.id = id;
-    }
     public void setPhoneNumber(String phoneNumber){
         this.phoneNumber = phoneNumber;
     }
     public void setEmail(String email){this.email = email;}
+    public void setStartDate(Date date){this.startDate = date;}
 
     @Override
     public String toString(){
