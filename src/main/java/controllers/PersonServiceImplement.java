9258c362c16773908d5c4f1c69a6212e6b5cd9d5
tkasa01 <kasatanya1@gmail.com>
2018-11-07 12:41:17 +0000

    Comments file

--- src/main/java/controllers/PersonServiceImplement.java
+++ src/main/java/controllers/PersonServiceImplement.java
@@ -1,106 +1,48 @@
 package main.java.controllers;
 
+import main.java.database.Database;
 import main.java.models.Person;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
 //import main.java.models.PersonResponse;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+public class PersonServiceImplement {
 
-@Path("/seller")
-@Consumes(MediaType.APPLICATION_JSON)
+    private static Map<String, Person> sellersMap = Database.getPeople();
 
-public class PersonServiceImplement implements PersonService{
-    private File file;
-
-    private static Map<Integer, Person> seller = new HashMap<Integer, Person>();
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String sayPlainText(){
-        return "Hello Postman from Person class!!!";
+    public PersonServiceImplement (){
+        sellersMap.put("Lola", new Person(1, "Lola","Smith","07346743435","lola@mail.com"));
+        sellersMap.put("Bob", new Person(2,"Bob", "Sherman", "034578934", "bob@mail.com"));
     }
 
-    @Override
-    @Path("/add")
-    @POST
-    public PersonResponse addPerson(Person p) {
+    public List<Person> getAllPeople() {
+        return new ArrayList<Person>(sellersMap.values());
+    }
+
+    public Person getPerson(String firstName){
+        return sellersMap.get(firstName);
+    }
+
+    public Person addPerson(Person p) {
         PersonResponse response = new PersonResponse();
-        if(seller.get(p.getId()) != null){
-            response.setStatus(false);
-            response.setMessage("The person already exists");
-            return  response;
+            p.setId(sellersMap.size() + 1);
+            sellersMap.put(p.getFirstName(), p);
+            return p;
+    }
+
+    public Person updatePerson(Person firstName){
+        if(firstName.getFirstName().isEmpty()){
+            return null;
         }
-        seller.put(p.getId(), p);
-        response.setStatus(true);
-        response.setMessage("The person is created.");
-        return response;
+        sellersMap.put(firstName.getFirstName(), firstName);
+        return firstName;
     }
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-        public String addMessage(){
-        return "POST works!";
+    public Person deletePerson(String firstName) {
+        return sellersMap.remove(firstName);
     }
 
-   @Override
-    @GET
-    @Path("/{id}/delete")
-    @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse deletePerson(@PathParam("id") int id) {
-        PersonResponse response = new PersonResponse();
-        if(seller.get(id) == null){
-            response.setStatus(false);
-            response.setMessage("The person doesn't found");
-            return response;
-        }
-        seller.remove(id);
-        response.setStatus(true);
-        response.setMessage("The person deleted.");
-        return response;
-    }
-
-
-    @GET
-    @Path("/home")
-    public InputStream getHome() throws FileNotFoundException {
-        File f = getFileFrom();
-        return  new FileInputStream(f);
-
-    }
-
-    private File getFileFrom() {
-        return file;
-    }
-
-    @GET
-    @Path("/{id}/getOne")
-    @Produces(MediaType.APPLICATION_XML)
-    public Person getPerson(@PathParam("id") int id){
-        Person seller1 = new Person("Lola","Smith", 1,"lola@gmail.com");
-        seller1.setPhoneNumber("07348522498");
-        return  seller1;
-
-    }
-
-    @Override
-    @GET
-    @Path("/{id}/getAll")
-    public Person[] getAllPeople() {
-        Set<Integer> ids = seller.keySet();
-        Person[] people = new Person[ids.size()];
-        int i = 0;
-        for(Integer id : ids){
-            people[i]= seller.get(id);
-            i++;
-        }
-        return people;
-    }
 }
 
